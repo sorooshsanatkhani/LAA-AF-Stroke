daf92eaed226117bb5fa8ae4ff4e6b2386b17780
Soroosh Sanatkhani <46576344+sorooshsanatkhani@users.noreply.github.com>
2023-03-14 10:21:45 -0400

    Update nonNewtonianDistVel.C

--- nonNewtonianDistVel/nonNewtonianDistVel.C
+++ nonNewtonianDistVel/nonNewtonianDistVel.C
@@ -5,7 +5,7 @@
 
 // Developed based on a sample nonNewtonianIcoFoam of OpenFOAM.
 
-/* Please cite our paper if you find this repository usefull:
+/* Please cite our paper if you find this repository useful:
 Sanatkhani, S., Nedios, S., Menon, P. G., Saba, S. F., Jain, S. K., Federspiel, W. J., & Shroff, S. G. (2023).
 Subject-specific factors affecting particle residence time distribution of left atrial appendage in atrial fibrillation:
 A computational model-based study. Front Cardiovasc Med, 10(1070498), 1-13. https://doi.org/10.3389/fcvm.2023.1070498 */
